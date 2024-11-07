209a7f01cf5dc6b3c91e0bc6c9bf9d156c398f70
Etheryen <lukasz.ciwoniuk@gmail.com>
2024-11-08 00:24:32 +0100

    feat: C1 set_3 memory-allocator

--- C1/set_3/memory-allocator/src/allocators.rs
+++ C1/set_3/memory-allocator/src/allocators.rs
@@ -1,4 +1,8 @@
-use std::alloc::{GlobalAlloc, Layout, System};
+use std::{
+    alloc::{GlobalAlloc, Layout, System},
+    collections::BTreeMap,
+    vec,
+};
 
 pub struct CustomAllocator;
 
@@ -37,5 +41,100 @@ impl BumpAllocator {
 }
 
 pub struct BuddyAllocator {
-    memory: Vec<u8>,
+    memory: BTreeMap<u32, Vec<u8>>,
+    max_size: usize,
+    curr_id: u32,
+}
+
+impl BuddyAllocator {
+    pub fn new(max_size: usize) -> Self {
+        Self {
+            memory: BTreeMap::new(),
+            max_size,
+            curr_id: 0,
+        }
+    }
+
+    fn size_to_allocate(size: usize) -> usize {
+        let mut power = 0_usize;
+
+        while 2_usize.pow(power.try_into().unwrap()) < size {
+            power += 1;
+        }
+
+        2_usize.pow(power.try_into().unwrap())
+    }
+
+    pub fn allocated_size(&self) -> usize {
+        self.memory.values().fold(0, |acc, curr| acc + curr.len())
+    }
+
+    pub fn alloc(&mut self, size: usize) -> Option<(u32, &mut [u8])> {
+        if size > self.max_size {
+            return None;
+        }
+
+        let memory_size = BuddyAllocator::size_to_allocate(size);
+
+        let free_memory_size = self.max_size - self.allocated_size();
+
+        if memory_size > free_memory_size {
+            return None;
+        }
+
+        self.memory.insert(self.curr_id, vec![0_u8; memory_size]);
+
+        self.curr_id += 1;
+
+        Some((self.curr_id - 1, self.memory.get_mut(&(self.curr_id - 1))?))
+    }
+
+    pub fn dealloc(&mut self, id: u32) {
+        self.memory
+            .remove(&id)
+            .expect("Deallocated unallocated memory");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bump_allocator() {
+        let mut bump_allocator = BumpAllocator::new(128);
+
+        let block = bump_allocator.alloc(4).unwrap();
+
+        block.fill(0xFF);
+    }
+
+    #[test]
+    fn test_buddy_allocator() {
+        let mut buddy_allocator = BuddyAllocator::new(128);
+
+        let (id, block) = buddy_allocator.alloc(4).unwrap();
+
+        block.fill(0xFF);
+
+        buddy_allocator.dealloc(id);
+
+        assert_eq!(buddy_allocator.allocated_size(), 0);
+
+        assert!(buddy_allocator.alloc(129).is_none());
+
+        assert!(buddy_allocator.alloc(33).is_some());
+        assert_eq!(buddy_allocator.allocated_size(), 64);
+
+        assert!(buddy_allocator.alloc(31).is_some());
+        assert_eq!(buddy_allocator.allocated_size(), 96);
+
+        assert!(buddy_allocator.alloc(15).is_some());
+        assert_eq!(buddy_allocator.allocated_size(), 112);
+
+        assert!(buddy_allocator.alloc(15).is_some());
+        assert_eq!(buddy_allocator.allocated_size(), 128);
+
+        assert!(buddy_allocator.alloc(1).is_none());
+    }
 }
--- C1/set_3/memory-allocator/src/main.rs
+++ C1/set_3/memory-allocator/src/main.rs
@@ -1,4 +1,6 @@
-use allocators::{BumpAllocator, CustomAllocator};
+use std::time::Instant;
+
+use allocators::{BuddyAllocator, BumpAllocator, CustomAllocator};
 
 mod allocators;
 
@@ -6,24 +8,42 @@ mod allocators;
 static A: CustomAllocator = CustomAllocator;
 
 fn main() {
-    let a = Box::new(9);
-    println!("a + a = {}", *a + *a);
-
-    test_bump_allocator();
-    test_buddy_allocator();
+    benchmark_rust_allocator();
+    benchmark_bump_allocator();
+    benchmark_buddy_allocator();
 }
 
-fn test_bump_allocator() {
-    let mut bump_allocator = BumpAllocator::new(128);
+fn benchmark_rust_allocator() {
+    let start = Instant::now();
+    for _ in 0..2048 {
+        // i32 is size 4 bytes
+        let _ = Box::new(213);
+    }
+    let rust_time = start.elapsed();
 
-    let block = bump_allocator.alloc(4).unwrap();
-    block.fill(0xFF);
-
-    println!("block: {:?}", block);
-
-    let max_u32 = u32::from_be_bytes([block[0], block[1], block[2], block[3]]);
-
-    assert_eq!(max_u32, u32::MAX);
+    println!("rust_allocator time: {:?}", rust_time);
 }
 
-fn test_buddy_allocator() {}
+fn benchmark_bump_allocator() {
+    let mut bump_allocator = BumpAllocator::new(8192);
+
+    let start = Instant::now();
+    for _ in 0..2048 {
+        let _ = bump_allocator.alloc(4);
+    }
+    let bump_allocator_time = start.elapsed();
+
+    println!("bump_allocator time: {:?}", bump_allocator_time);
+}
+
+fn benchmark_buddy_allocator() {
+    let mut buddy_allocator = BuddyAllocator::new(8192);
+
+    let start = Instant::now();
+    for _ in 0..2048 {
+        let _ = buddy_allocator.alloc(4);
+    }
+    let buddy_allocator_time = start.elapsed();
+
+    println!("buddy_allocator time: {:?}", buddy_allocator_time);
+}
